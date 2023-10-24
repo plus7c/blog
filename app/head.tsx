2a09d1fdeb813ab728753a746316887463bd0c85
plus7c <1959512279@qq.com>
2023-10-24 10:51:01 +0800

    add:site name

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Zplus7's Blog</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="width=device-width, initial-scale=1" name="Zplus7" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css"></link>
     </>
