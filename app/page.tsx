071f9ed87231bbfb0685decdd62c69044383dc27
plus7c <1959512279@qq.com>
2023-12-11 21:47:37 +0800

    try...catch

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,28 @@ async function getData() {
   // const res = await fetch("http://blog.zplus7.space/api/posts", {
   //   cache: "no-cache"
   // });
-
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const res = await fetch(`${apiBaseUrl}/api/posts`, {
-    cache: "no-cache"
-  });
-
-  if(!res.ok){
-    console.error(`Error fetching posts: ${res.status} - ${res.statusText}`);
-    throw new Error("获取博客列表失败")
+  try {
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const res = await fetch(`${apiBaseUrl}/api/posts`, {
+      cache: "no-cache"
+    });
+    if (!res.ok) {
+      console.error(`Error fetching posts: ${res.status} - ${res.statusText}`);
+      throw new Error("获取博客列表失败")
+    }
+    return res.json();
+  } catch (err) {
+    console.log(err)
   }
-  return res.json();
+
+
+
 }
 
 
 export default async function HomePage() {
   const data = await getData();
-  
+
   return (
     <>
       <Show postMetadata={data}></Show>
