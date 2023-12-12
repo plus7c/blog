e9f568d3ab21d3dd024969ba1ad68e832fa9f205
plus7c <1959512279@qq.com>
2023-12-12 23:26:04 +0800

    不用环境变量

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 
 const connect = async () => {
-    const apiDbUrl = process.env.MONGODB_URI;
+    //const apiDbUrl = process.env.MONGODB_URI;
+    const MONGODB_URI = "mongodb+srv://plus:plus999077@plus.dtrgijm.mongodb.net/blogData?retryWrites=true&w=majority"
+    const apiDbUrl = MONGODB_URI;
     mongoose.set('strictQuery', true);
     try{
         
