998994bded3447f9164b59ee79dd5e172c42c4e7
plus7c <1959512279@qq.com>
2023-12-13 00:21:45 +0800

    content修复

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -3,7 +3,7 @@ import Markdown from "markdown-to-jsx";
 //import matter from "gray-matter";
 //import getPostMetadata from "../../../components/getPostMetadata";
 import { notFound } from "next/navigation";
-
+import {getContent} from "@/utils/getContent"
 // const getPostContent = (slug: string) => {
 //   const folder = "posts/";
 //   const file = `${folder}${slug}.md`;
@@ -31,21 +31,21 @@ async function getData(id:string) {
   //   cache: "no-cache"
   // });
   
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const res = await fetch(`${apiBaseUrl}/api/posts/${id}`, {
-    cache: "no-cache"
-  });
+  // const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  // const res = await fetch(`${apiBaseUrl}/api/posts/${id}`, {
+  //   cache: "no-cache"
+  // });
 
-  if (!res.ok) {
-    return notFound();
-  }
-  return res.json();
+  // if (!res.ok) {
+  //   return notFound();
+  // }
+  // return res.json();
 }
 
 //生成元数据
 export async function generateMetadata({ params }:any) {
-  const post = await getData(params.id)
-  
+  //const post = await getData(params.id)
+  const post = await getContent(params.id)
   return {
     title:post.title,
     description:post.subtitle,
@@ -54,8 +54,8 @@ export async function generateMetadata({ params }:any) {
 
 
 const PostPage = async ({params}:any) => {
- 
-  const data = await getData(params.id);
+ const data = await getContent(params.id);
+//const data = await getData(params.id);
 
   return (
     <div>
---
+++ utils/getContent.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+import connect from "@/utils/db"
+import Post from "@/models/Post"
+
+export const getContent = async (id) => {
+    //fetch
+
+    
+    try {
+
+        await connect()
+        const posts = await Post.findById(id)
+        console.log("成功获取博客列表数据【API】",posts)
+        return posts;
+        //return new NextResponse(JSON.stringify(posts), { status: 200 })
+
+    } catch (err) {
+        return new NextResponse("调用获取博客列表失败【API】", { status: 500 })
+    }
+
+}
